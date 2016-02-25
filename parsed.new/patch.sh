aac1f749a9afb3c2e15cc9e648850df0a641248c
Leonid Sopov <lsopov@sopov.org>
2016-02-25 21:24:55 +0300

    update

--- parsed.new/patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-patch  < ../scripts/20-fix-parsed/001-fix-jude.patch
-perl ../scripts/20-fix-parsed/002-fix-first-word.pl
-perl ../scripts/20-fix-parsed/003-fix-i-tag.pl
-perl ../scripts/20-fix-parsed/004-fix-wrong-quot.pl
-patch  < ../scripts/20-fix-parsed/005-fix-text-for-quot.patch
-
