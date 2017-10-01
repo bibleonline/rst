b55d48370d047c361ebdddca7a24e017a5ed4aa0
Leonid Sopov <lsopov@sopov.org>
2017-10-02 01:48:54 +0300

    fix comma for 1mac 5.34

--- parsed/patch.sh
+++ parsed/patch.sh
@@ -25,3 +25,4 @@ perl -p -i -e 's/\&ndash;/\&mdash;/g;s/-/‐/g;' *dat
 patch  < ../scripts/20-fix-parsed/020-wront-text.patch
 patch  < ../scripts/20-fix-parsed/021-start-with-lowcase.patch
 patch  < ../scripts/20-fix-parsed/022-wrong-text-job36-24.patch
+patch  < ../scripts/20-fix-parsed/023-wrong-punctuation-1mac5-34.patch
