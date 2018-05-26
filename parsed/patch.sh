c75543484119e84a9c642461a8a57cead5a8fa25
Leonid Sopov <lsopov@sopov.org>
2018-05-26 22:32:28 +0300

    fix paragraphs

--- parsed/patch.sh
+++ parsed/patch.sh
@@ -31,3 +31,4 @@ patch  < ../scripts/20-fix-parsed/025-numbers.patch
 patch  < ../scripts/20-fix-parsed/026-wrong-text-1pe1-12.patch
 patch  < ../scripts/20-fix-parsed/027-wrong-text-luke-8-10.patch
 patch  < ../scripts/20-fix-parsed/028-hellip.patch
+patch  < ../scripts/20-fix-parsed/029-paragraphs.patch
